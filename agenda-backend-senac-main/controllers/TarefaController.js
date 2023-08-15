cf7c3f936e0c078c6ba107fde86faac35c1dcc56
IslaMCastro <133907365+IslaMCastro@users.noreply.github.com>
2023-08-15 19:31:19 -0300

    alterações

--- agenda-backend-senac-main/controllers/TarefaController.js
+++ agenda-backend-senac-main/controllers/TarefaController.js
@@ -17,6 +17,71 @@ const TarefaController = {
           res.status(400).json({ msg: "Não foi possivel salvar" });
         }
       },
+      //Ler - Read
+  getAll: async (req, res) => {
+    try {
+      const tarefa = TarefaModel.find();
+      // response.status(200).json({data: tarefa})
+      response.json(tarefa);
+    } catch (error) {
+      //em caso de erro retorne
+      console.log(error);
+      res.status(400).json({ msg: "Não foi possivel executar pesquisa" });
+    }
+  },
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const Tarefa = await TarefaModel.findById(id);
+      if (!Tarefa) {
+        res.status(404).json({ msg: "Tarefa não encontrada!" });
+        return;
+      }
+      res.json(Tarefa);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ msg: "Não foi possivel executar pesquisa" });
+    }
+  },
+  //Atualizar - Update
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const Tarefa = {
+        name: req.body.nome,
+      };
+      const TarefaAtualizada = await TarefaModel.findByIdAndUpdate(
+        id,
+        Tarefa
+      );
+      if (!TarefaAtualizada) {
+        res.status(404).json({ msg: "Tarefa não encontrada!" });
+        return;
+      }
+      res
+        .status(200)
+        .json({ TarefaAtualizada, msg: "Tarefa atualizada com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  //Remover - Delete
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const Tarefa = await TarefaModel.findById(id);
+      if (!Tarefa) {
+        res.status(404).json({ msg: "Tarefa não encontrada!" });
+        return;
+      }
+      const TarefaDeletada= await TarefaModel.findByIdAndDelete(id);
+      res
+        .status(200)
+        .json({ TarefaDeletada, msg: "Tarefa deletado com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = TarefaController;
